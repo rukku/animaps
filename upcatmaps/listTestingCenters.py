2b456e76855f58ecb74576385352a8de4701aece
RK Aranas <rk.aranas@gmail.com>
2014-08-13 23:02:18 +0800

    Add HTML files

--- upcatmaps/listTestingCenters.py
+++ upcatmaps/listTestingCenters.py
@@ -4,6 +4,24 @@ txtfile =open('old.index.txt')
 
 template = "                <li>%s</li>"
 
+'''
 for line in txtfile.readlines():
 		if "h4" in line:
 			print template % (line[26:-10])
+'''
+			
+for line in txtfile.readlines():
+		#if "h4" in line:
+		#	print template % (line[26:-10])
+		if "ul" in line:
+			print line
+		elif "<li><a" in line:
+			print line
+#		elif "menu-item" in line:
+#			continue
+#		elif "h4" in line:
+#			continue
+		elif "<div" in line:
+			print "                <div>"
+		elif "</div" in line:
+			print "                </div>"			
